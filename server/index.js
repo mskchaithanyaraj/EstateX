038b36b0f2e0fc4e5eb99ded5620ffd0aa14304d
mskchaithanyaraj <mskchaithanyaraj@gmail.com>
2025-06-16 15:37:05 +0530

    Created a test api route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 const app = express();
@@ -17,3 +18,5 @@ mongoose
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on port ${process.env.PORT || 5000}`);
 });
+
+app.use("/api/user", userRouter);
